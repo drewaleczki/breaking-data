5320118ecc0ed20f023af2d3d33f6271cbbefc24
ThiagoEvan <Tevangelista504@gmail.com>
2024-10-12 16:40:16 -0300

    Criando o menu utilizando o while

--- fase2-StartDataManagement/Main.py
+++ fase2-StartDataManagement/Main.py
@@ -1,15 +1,27 @@
+opcao_Menu = 0
 
-# Coletando os dados do produto
-nm_produto = input("Insira o nome do produto: ")
-vl_produto = float(input('Insira o valor do produto: '))
-tp_embalagem = input('Insira o tipo de embalagem: ')
+print("Bem vindo, poderia inserir os dados do produto abaixo\n")
 
-# Armazenando os dados no produto em um dicionário
-produto = { 
-    "id": {
-    'Nome':nm_produto, 
-    'Valor':vl_produto,  
-    'tp_embalagem':tp_embalagem
+while opcao_Menu != 2:
+
+    # Coletando os dados do produto
+    nm_produto = input("Insira o nome do produto: ")
+    vl_produto = float(input('Insira o valor do produto: '))
+    tp_embalagem = input('Insira o tipo de embalagem: ')
+
+    # Armazenando os dados no produto em um dicionário
+    produto = { 
+        "id": {
+        'Nome':nm_produto, 
+        'Valor':vl_produto,  
+        'tp_embalagem':tp_embalagem
+        }
     }
-}
-print('\nProduto cadastrado !')
+    print('\nProduto cadastrado !')
+
+    # Menu
+    opcao_Menu = int(input('''
+[1] - Cadastrar um novo produto
+[2] - Sair
+                       
+Escolha uma opção acima: '''))
